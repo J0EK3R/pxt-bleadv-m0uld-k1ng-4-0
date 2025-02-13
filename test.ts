1ca092df4c9718912bff6364e10f86955a393e59
J0EK3R <J0EK3R@users.noreply.github.com>
2025-02-13 14:22:45 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+input.onButtonPressed(Button.A, function () {
+    MK4.setChannelAndSend(MK4.Module.M1, MK4.Channel.A, 50)
+})
+input.onButtonPressed(Button.B, function () {
+    MK4.setChannelAndSend(MK4.Module.M1, MK4.Channel.A, 0)
+})
+MK4.init(MK4.Module.M1)
+MK4.setChannelOffset(MK4.Module.M1, MK4.Channel.A, 20)
